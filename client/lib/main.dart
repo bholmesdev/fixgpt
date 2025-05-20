8df77d8fe742e29e093bd3313059d13f5a84aedf
Ben Holmes <bholmesdev@gmail.com>
2025-05-19 21:11:39 -0400

    add nav bar with resizing on max width

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -49,13 +49,76 @@ class MyAppState extends ChangeNotifier {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+// Adding an _ means `private`
+class _MyHomePageState extends State<MyHomePage> {
+  var selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = GeneratorPage();
+      case 1:
+        page = Placeholder();
+      default:
+        throw UnimplementedError("No widget at $selectedIndex");
+    }
+    return LayoutBuilder(builder: (context, constraints) {
+      return Scaffold(
+        body: Row(
+          children: [
+            // Prevent iOS notch or status bars from covering the UI
+            SafeArea(
+              child: NavigationRail(
+                extended: constraints.maxWidth >= 600,
+                destinations: [
+                  NavigationRailDestination(
+                    icon: Icon(Icons.home),
+                    label: Text('Home'),
+                  ),
+                  NavigationRailDestination(
+                    icon: Icon(Icons.favorite),
+                    label: Text('Favorites'),
+                  ),
+                ],
+                selectedIndex: selectedIndex,
+                onDestinationSelected: (value) {
+                  // State mutation needs to be wrapped in `setState`.
+                  // Weird the compiler lets you assign without this...
+                  setState(() {
+                    selectedIndex = value;
+                  });
+                },
+              ),
+            ),
+            // Expanded = take remaining space. Greedy. flex 1
+            Expanded(
+              child: Container(
+                color: Theme.of(context).colorScheme.primaryContainer,
+                child: page,
+              ),
+            ),
+          ],
+        ),
+      );
+    });
+  }
+}
+
+class GeneratorPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
     var word = appState.randomWordPair;
 
     return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.primaryContainer,
       body: Padding(
         padding: const EdgeInsets.all(20.0),
         child: Center(
